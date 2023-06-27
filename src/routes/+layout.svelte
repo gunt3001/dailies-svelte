7b620ebf4e9b28a9d86ad6e9167c789a0d2c834e
TAKINA-PC\Gun <napat.gunt3001@gmail.com>
2023-06-27 23:07:12 +0800

    Tailwind based design for header + icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
     import "../app.css";
 </script>
 
-<slot />
+
+<div class="absolute">
+    <div class="h-80 w-screen bg-primary"></div>
+    <div class="min-h-screen bg-white dark:bg-gray-900">
+      
+    </div>
+</div>
+
+<div class="relative">
+    <slot></slot>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#9b51e0',
+            }
+        }
+    },
+    plugins: []
 };
