82e7a9b619bcb1d908d4f2bb402e42f0f3415c75
Napat R. on Hutao-Surface <napat.gunt3001@gmail.com>
2023-09-08 14:24:11 -0400

    Updated pill styling

--- src/lib/DatePill.svelte
+++ src/lib/DatePill.svelte
@@ -11,13 +11,15 @@
     type="button"
     class="flex flex-col shrink-0 mb-3 w-14 h-20 font-semibold rounded-lg justify-center items-center border dark:border-gray-800 dark:bg-gray-800 {hoverClasses}"
     class:dark:bg-purple-800={isActive}
+    class:bg-purple-500={isActive}
+    class:text-white={isActive}
     class:dark:text-gray-600={isIncomplete && !isActive}
 >
     <div class="text-center cursor-pointer">
         <div>
             <span
-                class:text-red-500={date.getDay() === 0 && (!isIncomplete || isActive)}
-                class:text-blue-500={date.getDay() === 6 && (!isIncomplete || isActive)}
+                class:text-red-500={date.getDay() === 0 && !isIncomplete && !isActive}
+                class:text-blue-500={date.getDay() === 6 && !isIncomplete && !isActive}
                 class:text-red-900={date.getDay() === 0 && isIncomplete && !isActive}
                 class:text-blue-900={date.getDay() === 6 && isIncomplete && !isActive}
             >
