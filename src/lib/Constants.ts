25fb0398619ea9009da98688f7b160183a7055d6
Gun on Hutao-Surface <napat.gunt3001@gmail.com>
2024-04-15 21:39:34 -1000

    Added constants for conguration.
    In thie future, this should instead be loaded from server's environment variable.

---
+++ src/lib/Constants.ts
@@ -0,0 +1,18 @@
+// Configuration constants
+
+// URL for the API endpoint
+export const API_ENDPOINT_URL = "./api";
+
+// The minimum content length at which warning highlights will be shown
+export const ENTRY_CONTENT_WARN_LENGTH = 180;
+
+// When fetching an entry from the server, determine how many extra entries 
+// before and after the requested entry to pre-fetch.
+export const NEARBY_ENTRIES_TO_PREFETCH = 7;
+
+// Whether to use the legacy API mode (compatible with classic Dailies app)
+export const LEGACY_API_MODE = false;
+
+// URL for the API endpoint when legacy mode is active
+export const LEGACY_API_ENDPOINT_URL = "";
+
