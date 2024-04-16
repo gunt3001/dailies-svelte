5bdd837be2bc0f192d70241fa2725acc71adb412
Gun on Hutao-Surface <napat.gunt3001@gmail.com>
2024-04-15 14:45:05 -1000

    Added ILegacyEntry type for compatibility with old Dailies API

--- src/lib/model/IEntry.ts
+++ src/lib/model/IEntry.ts
@@ -5,3 +5,12 @@ export default interface IEntry {
     mood: string;
     remarks: string;
 }
+
+// Legacy Entry type used by the old API
+export interface ILegacyEntry {
+    date: string;
+    content: string;
+    keyword: string;
+    mood: string;
+    remarks: string;
+}
