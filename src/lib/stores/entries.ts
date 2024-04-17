e94d067ff31b4ce0c5f4cf6c730cded8f1f2226b
Gun on Hutao-Surface <napat.gunt3001@gmail.com>
2024-04-16 17:33:37 -0700

    Update entries Store ability to fetch from date range

--- src/lib/stores/entries.ts
+++ src/lib/stores/entries.ts
@@ -2,7 +2,7 @@ import { API_ENDPOINT_URL, LEGACY_API_ENDPOINT_URL, LEGACY_API_MODE, NEARBY_ENTR
 import type IEntries from "$lib/model/IEntries";
 import type { ILegacyEntry } from "$lib/model/IEntry";
 import type IEntry from "$lib/model/IEntry";
-import { formatDate } from "$lib/utilities/dateUtilities";
+import { formatDate, iterateDates } from "$lib/utilities/dateUtilities";
 import { get, writable } from "svelte/store";
 
 function createEntriesStore() {
@@ -35,6 +35,38 @@ function createEntriesStore() {
                 return entries[dateStr];
             }
             return null;
+        },
+        getEntries: async function (from: Date, to: Date): Promise<IEntries> {
+            // Convert date to "YYYY-MM-DD" format in local timezone
+            const fromDateStr = formatDate(from);
+            const toDateStr = formatDate(to);
+
+            // If any of the requested date has no cached entry,
+            // fetch the entire thing from backend
+            let entries = get(this);
+            const hasMissingEntries =
+                iterateDates(from, to)
+                    .findIndex(x => !(formatDate(x) in entries)) != -1;
+            if (hasMissingEntries) {
+                const newEntries = await fetchEntries(from, to);
+
+                // Update values into store
+                for (const [key, value] of Object.entries(newEntries)) {
+                    update((entries) => {
+                        return { ...entries, [key]: value };
+                    });
+                }
+
+            }
+
+            // Return values from the updated store
+            entries = get(this);
+            let returnEntries = {} as IEntries;
+            for (let date of iterateDates(from, to).map(formatDate)) {
+                returnEntries[date] = entries[date];
+            }
+
+            return returnEntries;
         }
     };
 
@@ -47,25 +79,10 @@ export const entries = createEntriesStore();
 
 async function fetchEntry(date: Date): Promise<IEntries> {
 
-    // Fetch the entry from the backend
-    const newEntries: IEntries = {};
-
     if (LEGACY_API_MODE) {
-        // Legacy API only support pulling entries on a month-by-month basis
-        const month = date.getMonth() + 1;
-        const year = date.getFullYear();
-        const response = await fetch(`${LEGACY_API_ENDPOINT_URL}/Entries?year=${year}&month=${month}`);
-        const entries: ILegacyEntry[] = await response.json();
-
-        // Loop through every day of the requested month
-        // If the entry is not found, set it to null
-        // Otherwise, convert the entry to the new format
-        while (date.getMonth() === month - 1) {
-            const dateStr = formatDate(date);
-            const existingEntry = entries.find((entry) => entry.date.startsWith(dateStr));
-            newEntries[dateStr] = existingEntry ? convertToNewFormat(existingEntry) : null;
-            date.setDate(date.getDate() + 1);
-        }
+        // In legacy API mode, fetching a single entry already fetches entries
+        // for the entire month
+        return fetchEntries(date, date);
     }
     else {
         // Modern API should support pulling entries for specific dates
@@ -74,9 +91,56 @@ async function fetchEntry(date: Date): Promise<IEntries> {
             date.getMonth(), date.getDate() - NEARBY_ENTRIES_TO_PREFETCH);
         const endDate = new Date(date.getFullYear(),
             date.getMonth(), date.getDate() + NEARBY_ENTRIES_TO_PREFETCH);
-        const startDateStr = formatDate(startDate);
-        const endDateStr = formatDate(endDate);
-        
+        return fetchEntries(startDate, endDate);
+    }
+}
+
+/**
+ * Fetch multiple entries from the backend API
+ * 
+ * @param from start date
+ * @param to end date
+ * @returns a dictionary of entries
+ */
+async function fetchEntries(from: Date, to: Date): Promise<IEntries> {
+    // Fetch multiple entries from the backend
+    const newEntries: IEntries = {};
+
+    if (LEGACY_API_MODE) {
+        // Legacy API only support pulling entries on a month-by-month basis
+        // Fetch entries for all months in the range
+
+        // Create an array of year and month pairs
+        let monthsToFetch: { year: number, month: number }[] = [];
+        let date = new Date(from);
+        while (!(date.getFullYear() === to.getFullYear() &&
+            date.getMonth() === to.getMonth())) {
+            monthsToFetch.push({ year: date.getFullYear(), month: date.getMonth() + 1 });
+            date.setMonth(date.getMonth() + 1);
+        }
+
+        // Fetch entries for each month
+        for (const { year, month } of monthsToFetch) {
+            const response = await fetch(`${LEGACY_API_ENDPOINT_URL}/Entries?year=${year}&month=${month}`);
+            const entries: ILegacyEntry[] = await response.json();
+
+            // Loop through every day of the requested month
+            // If the entry is not found, set it to null
+            // Otherwise, convert the entry to the new format
+            date = new Date(year, month - 1, 1);
+            while (date.getMonth() === month - 1) {
+                const dateStr = formatDate(date);
+                const existingEntry = entries.find((entry) => entry.date.startsWith(dateStr));
+                newEntries[dateStr] = existingEntry ? convertToNewFormat(existingEntry) : null;
+                date.setDate(date.getDate() + 1);
+            }
+        }
+    }
+    else {
+        // Modern API should support pulling entries for specific dates
+        const startDateStr = formatDate(from);
+        const endDateStr = formatDate(to);
+
         const response =
             await fetch(`${API_ENDPOINT_URL}/entries?from=${startDateStr}&to=${endDateStr}`);
         const entries: IEntry[] = await response.json();
@@ -86,6 +150,7 @@ async function fetchEntry(date: Date): Promise<IEntries> {
     }
 
     return newEntries;
+
 }
 
 function convertToNewFormat(entry: ILegacyEntry): IEntry {
