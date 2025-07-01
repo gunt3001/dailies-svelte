d3d7c4ca76939849331493fd40d7ecd0379e58c4
Gun on Ume-Pro <napat.gunt3001@gmail.com>
2025-07-01 18:28:44 +0700

    lay out columns

---
+++ src/routes/list/+page.svelte
@@ -0,0 +1,12 @@
+<div class="container mx-auto flex flex-wrap gap-4">
+    <div
+        class="w-full md:w-64 p-8 drop-shadow overflow-hidde bg-white rounded border dark:bg-gray-900 dark:text-white dark:border-gray-800"
+    >
+        Sidebar
+    </div>
+    <div
+        class="flex-1 overflow-hidden p-8 drop-shadow bg-white rounded border dark:bg-gray-900 dark:text-white dark:border-gray-800"
+    >
+        Hello list view
+    </div>
+</div>
