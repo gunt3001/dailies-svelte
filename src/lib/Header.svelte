3fcb670c317d5a96290e02ce77d69a9e28ad25ed
Gun on Ume-Pro <napat.gunt3001@gmail.com>
2025-07-14 20:48:27 +0700

    fix: new search now update search results
    Svelte, by default, only rerun client-side load functions.
    Since our load runs server-side, it doesn't update the results by default.
    This workaround forces a reload using Svelte data attribute.
    Note that with this workaround we lose state, but it's the most straightforward solution.
    See: https://svelte.dev/docs/kit/load#Rerunning-load-functions

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -39,7 +39,7 @@
         </nav>
 
         <div class="md:ml-auto w-80 max-w-full">
-            <form action="/search" method="GET" class="relative">
+            <form action="/search" method="GET" class="relative" data-sveltekit-reload>
                 <div
                     class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
                 >
